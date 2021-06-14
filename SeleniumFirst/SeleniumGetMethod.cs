cf46bebca2bb8e5c9ed61c1ccd7a3b742a1f42e1
Nagihan Karpuzcu <Nagihan.Karpuzcu@wcbs.co.uk>
2021-06-14 11:19:30 +0100

    implemented GetMethod

--- SeleniumFirst/SeleniumGetMethod.cs
+++ SeleniumFirst/SeleniumGetMethod.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumFirst
 {
     class SeleniumGetMethod
     {
+        public static string Gettext(string element, PropertyType elementtype) {
+
+            if (elementtype == PropertyType.Id)
+                return PropertiesCollection.driver.FindElement(By.Id(element)).GetAttribute("value");
+            if (elementtype == PropertyType.Name)
+                return PropertiesCollection.driver.FindElement(By.Name(element)).GetAttribute("value");
+            else return String.Empty;
+
+        }
+
+        public static string GetTextFromDOL(String element, PropertyType elementtype) {
+
+            if (elementtype == PropertyType.Id)
+                return new SelectElement(PropertiesCollection.driver.FindElement(By.Id(element))).AllSelectedOptions.SingleOrDefault().Text;
+            if (elementtype == PropertyType.Name)
+                return new SelectElement(PropertiesCollection.driver.FindElement(By.Name(element))).AllSelectedOptions.SingleOrDefault().Text;
+            else return String.Empty;
+            
+        }
     }
 }
